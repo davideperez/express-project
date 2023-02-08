acfe7c83c664b31e10f4ab607ed39e096b8b7f4e
David Perez <david.ezequiel.pere@gmail.com>
2023-02-08 13:57:02 -0300

    sendFile example implemented, with use of path.join.

--- controllers/messages-controller.js
+++ controllers/messages-controller.js
@@ -1,9 +1,12 @@
+const path = require('path')
+
 function postMessage(req, res) {
     console.log("Updating Messages...")
 } 
 
 function getMessages(req, res) {
-    res.send("<ul><li> Hello David!! </li></ul>")
+        res.sendFile(path.join(__dirname, '..', 'public', 'beachChurch.jpg'))
+    //res.send("<ul><li> Hello David!! </li></ul>")
 } 
 
 module.exports = {
