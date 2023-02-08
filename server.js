fa01fe3ba69570fe312da44549ba7daca9460f20
David Perez <david.ezequiel.pere@gmail.com>
2023-02-08 12:26:01 -0300

    Routes implemented, middleware tracking ip on friends route added aas an example of custom middleware

--- server.js
+++ server.js
@@ -1,44 +1,22 @@
-//// Imports ////
-
 const express = require('express')
-const friendsController = require('./controllers/friends-controller')
-const messagesController = require('./controllers/messages-controller')
-
-//// SV Start ////
+const messagesRouter = require('./routes/messages-router')
+const friendsRouter = require('./routes/friends-router')
 
 const app = express()
 const PORT = 3000
-
-//// Data ////
-
-
-
-//// Middleware ////
-
-//#1
 
 app.use((req, res, next) => {
     const start = Date.now()
     next()
     const delta = Date.now() - start
-    console.log(`${req.method} ${req.url} ${delta} ms`)
-
+    console.log(`${req.method} ${req.baseUrl}${req.url} ${delta} ms`)
 })
-
-//#2
 
 app.use(express.json()) // middleware que convierte los req en jsons
 
-//// Routes ////
+app.use('/friends', friendsRouter)
+app.use('/messages', messagesRouter)
 
-app.get('/friends', friendsController.getFriends)
-app.get('/friends/:friendId',  friendsController.getFriend)
-app.post('/friends', friendsController.postFriend)
-
-app.get('/messages', messagesController.getMessages)
-app.post('/messages',messagesController.postMessage)
-
-//// Checks ////
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
 })
